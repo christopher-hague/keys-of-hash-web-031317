bab38f1862ef0b09cc4d1d25d5a15d479088a914
Chris Hague <christopher.a.hague@gmail.com>
2017-03-02 20:43:36 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+# This method should take an an undefined number of arguments, using
+# the splat operator, and return an array with every key from the hash
+# whose value matches the value(s) given as an argument.
+
+# if key == value passed as arg
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    self.select do |key, val|
+      arguments.include?(val)
+    end.keys
   end
 end
